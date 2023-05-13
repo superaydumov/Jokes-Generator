f965ff7735da6f97d621d8dda280567f50509ce9
Eldar Aidumov <superaydumov@me.com>
2023-05-13 14:39:31 +0300

    Initial commit. It's a test app to work with constraints in AutoLayout and Alerts.

---
+++ Helpers/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Jokes Generator
+//
+//  Created by Эльдар Айдумов on 12.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var backgroundGray: UIColor { UIColor (named: "Background-Gray") ?? UIColor.red }
+    static var pinky: UIColor { UIColor (named: "Pinky") ?? UIColor.systemPink }
+    static var greenGrass: UIColor { UIColor (named: "Green-Grass") ?? UIColor.green }
+}
